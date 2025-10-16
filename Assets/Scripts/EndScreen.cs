573361a3fc86446369583ed1f4c8d948b2cff605
Frayte-Dev <aleck.duke2002@gmail.com>
2025-10-16 13:40:42 -0400

    Started on animations for end screen in canvas

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class EndScreen : MonoBehaviour
 {
+    private EvidenceTracker evidenceTracker;
+
+    private void Start()
+    {
+        evidenceTracker = GameObject.Find("EvidenceTracker").GetComponent<EvidenceTracker>();
+    }
+
     public void goToEndScreen(bool wasKilled) { 
         
         //if killed, display the thing
