f8dc08efed041548cd52cf0eb1e95698f79a19c2
Diana <edvi.diana@gmail.com>
2025-10-16 16:53:34 -0400

    feat: added pick up and put down audio to handbook

--- Assets/Scripts/ActivateBook.cs
+++ Assets/Scripts/ActivateBook.cs
@@ -8,12 +8,14 @@ public class ActivateBook : MonoBehaviour, IPointerClickHandler
     [SerializeField] private GameObject handBookUI;
     [SerializeField] private Pages pages;
     [SerializeField] private DoorDisabler doorDisabler;
+    [SerializeField] private AudioSource pickUpSound;
     
     public void OnPointerClick(PointerEventData eventData)
     {
         // Reset the book and activate the handbook UI objects
         pages.ResetBook();
         doorDisabler.disableDoors();
+        pickUpSound.Play();
         foreach (Transform child in handBookUI.transform)
         {
             child.gameObject.SetActive(true);
